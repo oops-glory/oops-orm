dccfe96f9d255dad48677903ce6454254f8bc4eb
pleuvoir <pleuvior@foxmail.com>
2018-08-24 00:01:53 +0800

    :blush: update method

--- src/main/java/io/github/pleuvoir/sample/Client.java
+++ src/main/java/io/github/pleuvoir/sample/Client.java
@@ -4,17 +4,15 @@ import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map;
 import io.github.pleuvoir.handler.SimpleSqlHandler;
-import io.github.pleuvoir.handler.SqlHandler;
 import io.github.pleuvoir.sample.dao.UserDao;
 
 public class Client {
 
 	public static void main(String[] args) throws NoSuchMethodException, SecurityException {
-		SqlHandler handler = new SimpleSqlHandler();
-		Method selectAll = UserDao.class.getDeclaredMethod("selectByName", String.class);
+		Method selectByName = UserDao.class.getDeclaredMethod("selectByName", String.class);
 		Map<String, Object> map = new HashMap<>();
 		map.put("ename", "JONES");
-		handler.excuteSql(selectAll, map);
+		new SimpleSqlHandler().excuteSql(selectByName, map);
 	}
 	
 }
